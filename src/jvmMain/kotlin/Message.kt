093130c48e22487316f726be06e098129b9e0453
Bill <bill@outcubator.com>
2023-03-26 18:52:38 +0700

    Update message

--- src/jvmMain/kotlin/Message.kt
+++ src/jvmMain/kotlin/Message.kt
@@ -5,15 +5,10 @@ import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withStyle
 
 fun warningMessage(msg: String): AnnotatedString {
-    return buildAnnotatedString {
-        withStyle(
-            style = SpanStyle(
-                color = Color.Red
-            )
-        ) {
-            append(msg)
-        }
-    }
+    return coloredMessage(
+        msg = msg,
+        color = Color.Red
+    )
 }
 
 fun coloredMessage(msg: String, color: Color = Color.Black): AnnotatedString {
